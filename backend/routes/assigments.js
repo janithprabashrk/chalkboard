1deeeed3a218ce9bddf47416b6f9e49ca5f1ae5a
Nimnapathum <pathumeshan123@gmail.com>
2024-09-08 23:40:42 +0530

    Create update assignment route

--- backend/routes/assigments.js
+++ backend/routes/assigments.js
@@ -48,3 +48,29 @@ router.route("/get").get(async(req,res)=>{
     }
 
 });
+
+router.route("/update").put(upload.single('file'), async (req, res) => {
+    try {
+        const updateData = {
+            assignmentId: req.body.assignmentId,
+            description: req.body.description,
+            dueDate: req.body.dueDate,
+            pdfFile: req.file ? req.file.path : req.body.existingFilePath,
+            courseId: req.body.courseId
+        };
+
+        const updatedAssignment = await Assignment.findOneAndUpdate(
+            { assignmentId: req.body.assignmentId },
+            updateData,
+            { new: true }
+        );
+
+        if (!updatedAssignment) {
+            return res.status(400).json({ message: "Assignment not found" });
+        }
+
+        res.status(200).json({ message: "Assignment updated successfully!", updatedAssignment });
+    } catch (error) {  // Capture the error here
+        res.status(500).json({ message: "Error updating assignment", error: error.message });
+    }
+});
