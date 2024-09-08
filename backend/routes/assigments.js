7827187d1edc028b342e3d441f0f7757de9e0767
Nimnapathum <pathumeshan123@gmail.com>
2024-09-08 23:40:06 +0530

    Create retriew assignment route

--- backend/routes/assigments.js
+++ backend/routes/assigments.js
@@ -32,3 +32,19 @@ router.route("/add").post(upload.single('file'), async (req, res) => {
         res.status(500).json({ message: "Error adding assignment", error });
     }
 });
+
+router.route("/get").get(async(req,res)=>{
+    const { assignmentId } = req.body;
+    try{
+        const assigment = await Assignment.findOne({assignmentId});
+        if(!assigment){
+            return res.status(404).json({messag: " Assigment not found"});
+
+        }
+        res.status(200).json(assigment);
+        
+    }catch(error){
+        res.status(500).json({message: "Error retrieving assigment",error});
+    }
+
+});
