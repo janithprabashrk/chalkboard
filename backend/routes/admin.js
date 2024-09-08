370b525b056a267fc73565f903076dc948c260fc
D.N.Kodithuwakku <140165465+DilshaniNK@users.noreply.github.com>
2024-09-09 00:31:15 +0530

    create delete and update route for the admin

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -45,6 +45,46 @@ router.route("/").get(async (req, res) => {
     }
 });
 
+// Update admin
+router.route("/update").put(async (req, res) => {
+    const { adminId, newadminName, newadminAge, newadminPassword } = req.body;
+
+    try {
+        let hashedPassword = newadminPassword;
+        // Hash the new password
+        if(newadminPassword){
+        const salt = await bcrypt.genSalt(10);
+        hashedPassword = await bcrypt.hash(newadminPassword, salt);
+        }
+        const updatedAdmin = await Admin.findOneAndUpdate(
+            { adminId },
+            { adminName: newadminName, adminAge: newadminAge, adminPassword: hashedPassword },
+            { new: true }
+        );
+
+        if (updatedAdmin) {
+            res.status(200).send({ status: "Update successful", user: updatedAdmin });
+        } else {
+            res.status(404).send({ status: "Admin not found" });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ status: "Error updating admin", error: err.message });
+    }
+});
+
+// Delete Admin
+router.route("/delete").delete(async (req, res) => {
+    const { adminId } = req.body;
+
+    try {
+        await Admin.findOneAndDelete({ adminId });
+        res.status(200).send({ status: "User deleted" });
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send({ status: "Error deleting admin", error: err.message });
+    }
+});
 
 
 module.exports = router;
