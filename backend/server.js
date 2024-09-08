e34019b7d3d65ec9a524c593474ca65adf7f732e
D.N.Kodithuwakku <140165465+DilshaniNK@users.noreply.github.com>
2024-09-09 01:05:13 +0530

    create complete server.js file with all main routes that in our system

--- backend/server.js
+++ backend/server.js
@@ -25,11 +25,23 @@ connection.once("open", () => {
     console.log("Connected to MongoDB");
 
 })
-
+const studentRoute = require("./routes/student.js");
+const teacherRoute = require("./routes/teacher.js");
+const coursesRoute = require("./routes/courses.js");
+const AssigmentRoute = require("./routes/assigments.js");
 const AdminRouter = require("./routes/admin.js");
+
+app.use("/student",studentRoute);//http://localhost:8070/student
+app.use("/teacher",teacherRoute);//http://localhost:8070/teacher 
+app.use("/course",coursesRoute);//http://localhost:8070/course
+app.use("/assignment",AssigmentRoute);//http://localhost:8070/assigment
 app.use("/admin",AdminRouter);//http://localhost:8070/admin
 
 app.listen(PORT, () =>{
     console.log(`Server Runs on ${PORT} PORT `);
 
 })
+
+
+// Serve the uploads folder as static
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
