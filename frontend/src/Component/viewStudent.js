c477f24d92f5fc8d63d32e0c569eb4e26d5e36c7
Nuwan Chanuka <140190472+Nuwanchanuka@users.noreply.github.com>
2024-09-08 19:58:33 +0530

    create View student

---
+++ frontend/src/Component/viewStudent.js
@@ -0,0 +1,29 @@
+// import React, {useState,useEffect} from 'react'
+// import axios from 'axios';
+
+// function ViewStudent() {
+// const [students, setstudents] = useState([]);
+// useEffect(()=>{
+//         function getStudent(){
+//             axios.get("http://localhost:8070/student/").then((res)=>{
+//                 console.log(res.data);
+//                 setstudents(res.data);
+//             }).catch((err)=>{
+//                 alert(err.massage);
+
+//             })
+//         }
+//         getStudent();
+// },[])
+//   return (
+//     <div className="container">
+//         <h1>All student</h1>
+
+
+
+         
+//     </div>
+//   )
+// }
+
+// export default ViewStudent;
